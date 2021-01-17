cbe5c1188f5070aa9c192f63ea287679df86ee1c
ledaithang <62004863+ledaithang@users.noreply.github.com>
2021-01-17 09:57:46 -0800

    Add files via upload

---
+++ background.js
@@ -0,0 +1,8 @@
+x = document.querySelector('.menu-icon');
+x.addEventListener('click', function(){
+    document.querySelector('').classList.toggle('');
+})
+y = document.querySelector('.User');
+y.addEventListener('click', function(){
+    document.querySelector('.InOut').classList.toggle('down-up');
+})
